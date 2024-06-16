a6c44a9dc0d205edd99aa56be9f26dd88e8fd07f
Aki0x137 <akshay.developer.007@gmail.com>
2024-06-16 17:20:31 +0530

    first rust implementation with no handling for unsafe code

--- Rust/concurrent-non-blocking-queue/src/main.rs
+++ Rust/concurrent-non-blocking-queue/src/main.rs
@@ -1,37 +1,74 @@
-use std::rc::Rc;
+use std::{borrow::Borrow, ptr, sync::{atomic::{AtomicPtr, Ordering}, Arc, Mutex}};
 use std::cell::RefCell;
 
 struct Node {
     value: i64,
-    ptr: Option<Rc<RefCell<Node>>>,
+    next: AtomicPtr<Node>,
 }
 
 impl Node {
-    fn new(value: i64) -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Self { value, ptr: None }))
+    fn new(value: i64) -> Arc<RefCell<Self>> {
+        Arc::new(RefCell::new(Self { value, next: AtomicPtr::new(ptr::null_mut()) }))
     }
 }
 
 struct Queue {
-    head: Rc<RefCell<Node>>,
-    tail: Rc<RefCell<Node>>,
+    head: Arc<AtomicPtr<Node>>,
+    tail: Arc<AtomicPtr<Node>>,
 }
 
 impl Queue {
     fn new() -> Self {
-        let node = Node::new(0);
+        let node: Arc<RefCell<Node>> = Node::new(0);
         Self {
-            head: Rc::clone(&node),
-            tail: node,
+            head: Arc::new(AtomicPtr::new(node.as_ptr())),
+            tail: Arc::new(AtomicPtr::new(node.as_ptr())),
         }
     }
 
     fn enqueue(&mut self, value: i64) {
         let new_node = Node::new(value);
+        let mut tail;
+
+        loop {
+            let tail = self.tail.load(Ordering::SeqCst);
+            let next = (*tail).next.load(Ordering::SeqCst);
+
+            if tail == self.tail.load(Ordering::SeqCst) {
+                if next.is_null() {
+                    if (*tail).next.compare_exchange(next, new_node.as_ptr(), Ordering::SeqCst, Ordering::SeqCst).is_ok() {
+                        self.tail.compare_exchange(tail, new_node.as_ptr(), Ordering::SeqCst, Ordering::SeqCst);
+                        break;
+                    }
+                } else {
+                    self.tail.compare_exchange(tail, next, Ordering::SeqCst, Ordering::SeqCst);
+                }
+            }
+        }
         
-        // Update the tail pointer to the new node, and adjust other pointers as needed.
-        self.tail.borrow_mut().ptr = Some(Rc::clone(&new_node));
-        self.tail = new_node;
+        self.tail.compare_exchange(tail, new_node.as_ptr(), Ordering::SeqCst, Ordering::SeqCst);
+    }
+
+    fn dequeue(&mut self) -> Option<i64> {
+        loop {
+            let head = self.head.load(Ordering::SeqCst);
+            let tail = self.tail.load(Ordering::SeqCst);
+            let next = (*head).next.load(Ordering::SeqCst);
+
+            if head == self.head.load(Ordering::SeqCst) {
+                if head == tail {
+                    if next.is_null() {
+                        return None;
+                    }
+                    self.tail.compare_exchange(tail, next, Ordering::SeqCst, Ordering::SeqCst).ok();
+                } else {
+                    if self.head.compare_exchange(head, next, Ordering::SeqCst, Ordering::SeqCst).is_ok() {
+                        return Some((*next).value);
+                    }
+                        
+                }
+            } 
+        }
     }
 }
 
